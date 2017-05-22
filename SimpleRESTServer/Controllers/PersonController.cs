e2637ed80bd290a1fee8a02d255d28368b288deb
mauricio <4442657@gmail.com>
2017-05-22 11:37:21 -0400

    creacion de repositorio

---
+++ SimpleRESTServer/Controllers/PersonController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SimpleRESTServer.Models;
+
+namespace SimpleRESTServer.Controllers
+{
+    public class PersonController : ApiController
+    {
+        // GET: api/Person
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Person1", "Person2" };
+        }
+
+        // GET: api/Person/5
+        public Person Get(long id)
+        {
+            PersonPersistence pp = new PersonPersistence();
+            Person person = pp.getPerson(id);
+            
+            return person;
+        }
+
+        // POST: api/Person
+        public HttpResponseMessage Post([FromBody]Person value)
+        {
+            PersonPersistence pp = new PersonPersistence();
+            long id;
+            id = pp.savePerson(value);
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Created);
+            response.Headers.Location = new Uri(Request.RequestUri, String.Format("person/{0}", id));
+            return response;
+
+        }
+
+        // PUT: api/Person/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Person/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SimpleRESTServer/PersonPersistence.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SimpleRESTServer.Models;
+using MySql.Data;
+
+namespace SimpleRESTServer
+{
+    public class PersonPersistence
+    {
+        private MySql.Data.MySqlClient.MySqlConnection conn;
+
+        public PersonPersistence()
+        {
+            string myConnectionString;
+            myConnectionString = "server= localhost ;uid=root; pwd=Manager260198; database=employeedb;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+
+            }
+        }
+
+        public Person getPerson(long ID)
+        {
+            Person p = new Person();
+            MySql.Data.MySqlClient.MySqlDataReader mySQLReader = null;
+
+            String sqlString = "SELECT * FROM tblpersonnel WHERE ID=" + ID.ToString();
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+
+            mySQLReader = cmd.ExecuteReader();
+            if (mySQLReader.Read())
+            {
+                p.ID = mySQLReader.GetUInt32(0);
+                p.FirstName = mySQLReader.GetString(1);
+                p.LastName = mySQLReader.GetString(2);
+                p.PayRate = mySQLReader.GetUInt32(3);
+                p.StartDate = mySQLReader.GetDateTime(4);
+                p.EndDate = mySQLReader.GetDateTime(5);
+                return p;
+
+
+            }
+
+            else
+            {
+                return null;
+            }
+
+
+
+        }
+        public long savePerson(Person personToSave)
+        {
+            string sqlString = "Insert into tblPersonnel(FirstName, LastName, PayRate, StartDate, EndDate) VALUES('" + personToSave.FirstName + "','" + personToSave.LastName + "'," + personToSave.PayRate + ",'" + personToSave.StartDate.ToString("yyyy-MM-dd HH:MM:ss") + "','" + personToSave.EndDate.ToString("yyyy-MM-dd HH:MM:ss") + "')";
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+           cmd.ExecuteNonQuery();
+            long id = cmd.LastInsertedId;
+            return id;
+        }
+
+    }
+}
